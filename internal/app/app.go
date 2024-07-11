400314137fd0af3e7f4d0b724039a247994b513f
Бароян Гиоргий <giorgiy_03@mail.ru>
2024-07-11 21:22:10 +0300

    init

--- internal/app/app.go
+++ internal/app/app.go
@@ -8,52 +8,25 @@ import (
 	"myapp/internal/handler"
 	"myapp/internal/usecase"
 	"myapp/internal/usecase/repository"
-	"myapp/pkg/ldap"
-	"myapp/pkg/oracle"
 	"myapp/pkg/postgres"
-	"myapp/pkg/samba"
 )
 
 func Run(cfg *config.Config) {
-
-	// Ldap
-	ldapConn, err := ldap.ConnectToServerLDAP(cfg.Ldap)
-	if err != nil {
-		log.Error().Err(err).Msg("app - Run - ldap.ConnectToServerLDAP")
-	}
-	defer ldapConn.Close()
-
-	// Samba
-	smbSession, err := samba.New(cfg.Samba)
-	if err != nil {
-		log.Error().Err(err).Msg("app - Run - samba.New")
-	}
-	defer smbSession.Logoff()
-
-	// Repository
-	ora, err := oracle.New(cfg.Oracle)
-	if err != nil {
-		log.Error().Err(err).Msg("app - Run - oracle.New")
-	}
-	defer ora.Close()
-
 	pg, err := postgres.New(cfg.Postgres)
 	if err != nil {
 		log.Fatal().Err(err).Msg("app - Run - Postgres.New")
 	}
 	defer pg.Close()
 
-	callDetailsPostgresRepo := repository.NewScreenRecorderPostgres(pg)
+	postgresRepo := repository.NewScreenRecorderPostgres(pg)
 
 	// Use case
-	authUseCases := usecase.NewAuthUseCases(ldapConn, cfg.Auth.Key)
-	screenRecorderUseCases := usecase.NewScreenRecorderCases(cfg.Links.Audio, smbSession, callDetailsPostgresRepo, callDetailsOracleRepo)
+	screenRecorderUseCases := usecase.NewScreenRecorderCases(postgresRepo)
 
 	// HTTP Server
 	router := gin.Default()
 
 	handler.NewRouter(router, *screenRecorderUseCases, authUseCases)
 
-	router.Run(fmt.Sprintf("localhost:%d", cfg.Http.Port))
-
+	router.Run(fmt.Sprintf(":%d", cfg.Http.Port))
 }
